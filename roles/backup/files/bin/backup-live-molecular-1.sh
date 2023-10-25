5189d9f97914ec1efb79ea766739bf21c66b4425
Mats Bovin <mats.bovin@gmail.com>
2023-10-25 15:14:11 +0200

    [backup] Tweak mol-mod backup

--- roles/backup/files/bin/backup-live-molecular-1.sh
+++ roles/backup/files/bin/backup-live-molecular-1.sh
@@ -4,7 +4,7 @@ TODAY=$(date +%y%m%d)
 
 echo "== mol-mod, date: $TODAY"
 echo "- rsync backups: $(date)"
-rsync -a live-molecular-1:mol-mod/backups /backup/data/mol-mod/
+rsync -a live-molecular-1:mol-mod/backups/latest/ /backup/data/mol-mod/latest/
 echo "- rsync imports: $(date)"
 rsync -a live-molecular-1:mol-mod/imports /backup/data/mol-mod/
 echo "- done: $(date)"
