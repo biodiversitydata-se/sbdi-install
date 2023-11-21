3ecb8d9585897c0af812d9692479b4824c246e9f
Mats Bovin <mats.bovin@gmail.com>
2023-11-21 08:42:26 +0100

    Remove backup-docker_nfs-db.sh

--- roles/backup/files/bin/nightly.sh
+++ roles/backup/files/bin/nightly.sh
@@ -10,15 +10,6 @@
 /backup/bin/backup-swarm-mysql.sh wordpress_docs wordpress_mysqldb-wordpress-docs wordpress
 /backup/bin/backup-swarm-mysql.sh wordpress_tools wordpress_mysqldb-wordpress-tools wordpress
 
-/backup/bin/backup-docker_nfs-db.sh collectory db_data_collectory
-/backup/bin/backup-docker_nfs-db.sh image-service db_data_image-service
-/backup/bin/backup-docker_nfs-db.sh logger db_data_logger
-/backup/bin/backup-docker_nfs-db.sh spatial-service db_data_spatial-service
-/backup/bin/backup-docker_nfs-db.sh specieslists db_data_specieslists
-/backup/bin/backup-docker_nfs-db.sh wordpress_main wordpress_main_db
-/backup/bin/backup-docker_nfs-db.sh wordpress_docs wordpress_docs_db
-/backup/bin/backup-docker_nfs-db.sh wordpress_tools wordpress_tools_db
-
 /backup/bin/backup-docker_nfs-dir.sh collectory data_collectory
 /backup/bin/backup-docker_nfs-dir.sh wordpress_main wordpress_main_html
 /backup/bin/backup-docker_nfs-dir.sh wordpress_docs wordpress_docs_html
